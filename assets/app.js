7ad3a96b287eca2a225ca3ee119f77cd7fcec517
Alex <ahutch006@gmail.com>
2017-05-21 12:27:35 -0700

    add recipe snippets

--- assets/app.js
+++ assets/app.js
@@ -7,5 +7,45 @@ $(function() {
       $('#nav').css({'background-color': 'rgba(0, 0, 0, 0)'})
     }
   })
-   $("#my-menu").mmenu({});
+
+  if (window.location.pathnamne !== '/') {
+    console.log('not home');
+    $('#nav').css({'background-color': 'rgba(55, 55, 55, 0.5)'})
+  }
+
+  $('ul.product-tabs').each(function(){
+    var active, content, links = $(this).find('a');
+    active = links.first().addClass('active');
+    content = $(active.attr('href'));
+    links.not(':first').each(function () {
+      $($(this).attr('href')).hide();
+    });
+    $(this).find('a').click(function(e){
+      active.removeClass('active');
+      content.hide();
+      active = $(this);
+      content = $($(this).attr('href'));
+      active.addClass('active');
+      content.show();
+      return false;
+    });
+  });
+
+  $('ul.featured-wines__nav').each(function(){
+    var active, content, links = $(this).find('a');
+    active = links.first().addClass('active');
+    content = $(active.attr('href'));
+    links.not(':first').each(function () {
+      $($(this).attr('href')).hide();
+    });
+    $(this).find('a').click(function(e){
+      active.removeClass('active');
+      content.hide();
+      active = $(this);
+      content = $($(this).attr('href'));
+      active.addClass('active');
+      content.show();
+      return false;
+    });
+  });
 });
