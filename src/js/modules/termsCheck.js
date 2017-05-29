6318d32dd83e00c4159c96bdd21cef57c9d803b9
Jerry Chai <jerry@gooddogdesign.com>
2017-05-29 01:23:57 -0700

    fix terms check submit bugs

--- src/js/modules/termsCheck.js
+++ src/js/modules/termsCheck.js
@@ -1,44 +1,23 @@
 // *************************************
 //
 //   Terms check
-//   -> Prevents form submit unless T&C checkbox is not checked
+//   -> Prevents form submit unless T&C checkbox is checked
 //
 // *************************************
 var termsCheck = (function() {
 
-  function requireChecked($formId) {
-    $(document).submit(function(e){
-      var $checkbox = $(document).find($formId + ' .terms-check input:checkbox');
-      var $label = $(document).find($formId + ' .terms-check label');
+  function requireChecked($toSubmit) {
+    $toSubmit.submit(function(e){
+      var $checkbox = $(this).find('.terms-check input:checkbox');
+      var $label = $(this).find('.terms-check label');
       if (!$checkbox.is(':checked')) {
-        console.log($formId)
         e.preventDefault();
         $label.css({color:'red'});
       }
     });
   }
 
-  // only activates age check if on sign up page
-  if ($('#create-account')[0]) {
-    requireChecked('#create_customer');
-  }
-
-  // only activates cart age check if cart is visible
-  var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-      if ($('#CartDrawer').is(':visible')) {
-        requireChecked('#cart-form');
-      }
-    });
-  });
-
-  var observerConfig = {
-    attributes: true,
-    childList: true,
-    characterData: true
-  };
-
-  var targetNode = document.getElementById('CartContainer');
-  observer.observe(targetNode, observerConfig);
+  requireChecked($('#create_customer'));
+  requireChecked($('#CartContainer'));
 
 })();
