4499d5131274ff2f09aca700da89ad84a594f052
Alex <ahutch006@gmail.com>
2017-06-02 21:42:51 -0700

    front and back form validation

--- assets/site.js
+++ assets/site.js
@@ -63,49 +63,54 @@ var variableNav = (function() {
 //   -> check for zip code tax info
 //
 // *************************************
-var zipCheck = (function() {
-  var $body = $(document.body);
-  console.log($('.btn--secondary.btn--full.cart__checkout'));
+const zipCheck = (function() {
+  const $body = $(document.body);
   $('.btn--secondary.btn--full.cart__checkout').attr('id', 'checkout-btn');
-  // $body.on('submit', '.cart.ajaxcart', function(e) {
-  //   e.preventDefault();
-  //   var id = parseInt(window.localStorage.getItem('taxId'));
-  //   $.ajax({
-  //     url: '/cart.js',
-  //     contentType: 'application/json',
-  //     dataType: 'json'
-  //   })
-  //     .then(function(data) {
-  //       return data.items.filter(function(data) {
-  //         return data.variant_id === id;
-  //       });
-  //     })
-  //     .done(function(data) {
-  //       if (data.length === 0) {
-  //         console.log('tax id not present');
-  //         return false;
-  //       } else {
-  //         console.log('submitting...');
-  //       }
-  //     });
-  // });
   $body.on('submit', '.cart.ajaxcart', function(e) {
     e.preventDefault();
     $('.btn--secondary.btn--full.cart__checkout')
       .attr('disabled', 'disabled')
       .addClass('disabled');
-    var zipCode = $('#zip-input').val();
+    $('#warning-container').empty();
+
+    const zipCode = $('#zip-input').val();
     const zipRegex = /^\d{5}$/;
     const isValidZip = zipRegex.test(zipCode);
-
-    if (isValidZip) {
+    const id = parseInt(window.localStorage.getItem('taxId'));
+    console.log('ID', id);
+    if (id) {
       $.ajax({
         url: '/cart.js',
         contentType: 'application/json',
         dataType: 'json'
       })
         .then(function(data) {
-          var total = JSON.parse(data.total_price);
+          return data.items.filter(function(data) {
+            return data.variant_id === id;
+          });
+        })
+        .done(function(data) {
+          if (data.length === 0) {
+            $('#warning-container').empty();
+            $('#warning-container').append(
+              '<p class="warning">please enter a valid zip code to continue!!</p>'
+            );
+            $('.btn--secondary.btn--full.cart__checkout')
+              .removeAttr('disabled', 'disabled')
+              .removeClass('disabled');
+            return false;
+          } else {
+            $('.cart.ajaxcart')[0].submit();
+          }
+        });
+    } else if (isValidZip && !id) {
+      $.ajax({
+        url: '/cart.js',
+        contentType: 'application/json',
+        dataType: 'json'
+      })
+        .then(function(data) {
+          const total = JSON.parse(data.total_price);
           return $.ajax({
             method: 'POST',
             url: 'http://fb426d6e.ngrok.io/compliance/check',
@@ -114,9 +119,13 @@ var zipCheck = (function() {
         })
         .then(function(response) {
           if (!response.data.compliant) {
-            $('.zip-check').append(
+            $('#warning-container').empty();
+            $('#warning-container').append(
               '<p class="warning">we do not currently ship to that location.</p>'
             );
+            $('.btn--secondary.btn--full.cart__checkout')
+              .removeAttr('disabled')
+              .removeClass('disabled');
             return;
           }
           localStorage.setItem('taxId', response.data.id);
@@ -133,7 +142,8 @@ var zipCheck = (function() {
           });
         });
     } else {
-      $('.zip-check').append(
+      $('#warning-container').empty();
+      $('#warning-container').append(
         '<p class="warning">please enter a valid zip code to continue</p>'
       );
     }
