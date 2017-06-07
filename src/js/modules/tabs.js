d471500dfece74e2a32c91c5e8c5a46812a2e7e4
Alex <ahutch006@gmail.com>
2017-06-06 22:37:00 -0700

    add tab js to compiled file

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,37 @@
+$(function() {
+  $('ul.product-tabs').each(function() {
+    var active, content, links = $(this).find('a');
+    active = links.first().addClass('active');
+    content = $(active.attr('href'));
+    links.not(':first').each(function() {
+      $($(this).attr('href')).hide();
+    });
+    $(this).find('a').click(function(e) {
+      active.removeClass('active');
+      content.hide();
+      active = $(this);
+      content = $($(this).attr('href'));
+      active.addClass('active');
+      content.show();
+      return false;
+    });
+  });
+
+  $('ul.featured-wines__nav').each(function() {
+    var active, content, links = $(this).find('a');
+    active = links.first().addClass('active');
+    content = $(active.attr('href'));
+    links.not(':first').each(function() {
+      $($(this).attr('href')).hide();
+    });
+    $(this).find('a').click(function(e) {
+      active.removeClass('active');
+      content.hide();
+      active = $(this);
+      content = $($(this).attr('href'));
+      active.addClass('active');
+      content.show();
+      return false;
+    });
+  });
+});
